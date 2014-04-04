26e1d1e3b44d5cc68b2215aee37601c1fe0a4a34
Christian Torres <chtorrez@gmail.com>
2014-04-04 17:18:51 -0600

    Usando la directiva static y el directorio static dentro de un app para colectar los archivos de estilos, imagenes o javascript.

--- assets_django/settings.py
+++ assets_django/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'resources',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +86,9 @@ STATIC_URL = '/static/'
 # by @chrux
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates')
+)
--- assets_django/urls.py
+++ assets_django/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'assets_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^cascade', TemplateView.as_view(template_name='resources/cascade.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
